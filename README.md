f4803731145e12f085f753bd13e15d72ad2f03eb
Zeyu-Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-04-02 15:25:58 -0600

    Worked on menu options

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Python Game
-About 
--------
+## About 
+
 This is a Python text-based game
+
+## Usage
+
+Navigate using ternimal
+If you are lost, use the help option in main menu
+
+** Still in progress
--- main.py
+++ main.py
@@ -12,9 +12,11 @@ This is a text-based Python game
 # import modules
 import json
 import os
-
+import time
+import sys
 
 class Print:
+    """ print actions """
 
     def instant(self, text):
 
@@ -24,32 +26,32 @@ class Print:
 
     def slow(self, text):
 
-        print(text)
+        for char in text:
+
+            # TODO: optimize 
+
+            if char == "\"":
+                continue
+
+            sys.stdout.write(char)
+
+            time.sleep(.03)
+
         return 0
 
 
 class Event:
     """All events"""
 
-    # def __init__(self):
-    #     action = str(input("What do you do?").split())
-
     def beginning(self):
 
-        possible = ["look", "crouch", "observe", "see", "go", ""]
+        # TODO: starting adventure
 
-        if (action in possible):
-
-            # switch statsement
-
-            return 0
-
-        else:
-
-            return "not possible"
+        return 0
 
 
 class Cover():
+    """ title screen """
 
     def title(self):
         print("""
@@ -80,18 +82,46 @@ class Cover():
 
         load = stats()
 
-        Pr.instant("NEW GAME")
+        test = True
 
-        Pr.instant("HELP")
+        while True:
 
-        if not load:
-            Pr.instant("LOAD GAME")
+            Pr.instant("NEW GAME")
 
-        Pr.instant("QUIT")
+            Pr.instant("HELP")
 
-        option = str(input("> ")).split().upper()
+            if not load:
+                Pr.instant("LOAD GAME")
 
-        return option
+            Pr.instant("QUIT")
+
+            # user option
+            option = user_input()
+
+            if option == "QUIT":
+
+                Pr.slow("Thanks for playing")
+                return "BREAK"
+
+            elif option == " HELP":
+
+                Co.help()
+
+
+
+        return 0
+
+    def help(self):
+
+        print("\n To start this game, enter \"new game\"".center(20))
+        print("If you have previously played this game,".center(20))
+        print("if you don't see the option, report the bug in the following url:".center(20))
+        print("https://github.com/Zeyu-Li/text_game/issues".center(20))
+
+def user_input():
+
+    return (str(input("> ").split())).upper()
+
 
 
 def stats():
@@ -133,7 +163,7 @@ def stats():
 
 def start():
 
-    print(""""
+    intro = """"
     2019: You fall into a nuclear dump site
 
     Yearning for sunlight to photosynthesize,
@@ -143,37 +173,32 @@ def start():
     As you wonder around on your studded roots,
     you find a small hole
 
-    """)
+    With limited time on your leaves, what do you do?
+    """
+
+    Pr.slow(intro)
 
     Ev.beginning()
 
-
-def init():
-
-    # global vars
-    global area
-    global hp
-    global attack
-    global special
-
-    # shortened versions
-    Ev = Event()
-    Co = Cover()
-    Pr = Print()
+# init vars
+global area
+global hp
+global attack
+global special
+Ev = Event()
+Co = Cover()
+Pr = Print()
 
 
 def main():
 
-    init()
+    option = Co.title()   
 
-    option = Co.title()
+    if option == "BREAK":
 
-    if not option:
-
-        Pr.slow("Thanks for playing")
         return 0
 
-    start()
+    # start()
     return 0
 
 # system calls name
